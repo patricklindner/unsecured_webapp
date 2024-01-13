de4522205527903027dc6298db6da04ce45ddf9f
patrick <patricklindner@outlook.com>
2024-01-13 21:41:41 +0100

    application works

--- src/main/java/org/group7/unsecured/TestController.java
+++ src/main/java/org/group7/unsecured/TestController.java
@@ -1,23 +1,13 @@
 package org.group7.unsecured;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/secured")
+//@RestController
+//@RequestMapping("/secured")
 public class TestController {
 
-    @GetMapping
-    public String securedResource() {
-        return "This is secured content";
-    }
-
-
     @PostMapping
-    public void postForm(@RequestBody String privateData) {
+    public void postForm(@RequestParam String privateData) {
         System.out.println(privateData);
     }
 
--- src/main/java/org/group7/unsecured/ViewController.java
+++ src/main/java/org/group7/unsecured/ViewController.java
@@ -2,12 +2,26 @@ package org.group7.unsecured;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ViewController {
 
     @GetMapping("/login")
-    public String index() {
+    public String login() {
         return "login";
     }
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "secured";
+    }
+
+    @PostMapping("/secured")
+    public String secret(@RequestParam String privateData) {
+        System.out.println("Processing private data received from user [" + privateData + "]");
+        return "secured";
+    }
+
 }
